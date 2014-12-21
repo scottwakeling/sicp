abcb04d52a8f989551454b7f6cefffb63120db4b
Scott Wakeling <scott@diskfish.org>
2014-12-21 00:41:50 +0000

    prime?

--- ch1/ch1-2.scm
+++ ch1/ch1-2.scm
@@ -342,4 +342,22 @@ sine (12.15)
 ; 4 times
 
 ; 1.2.6 Testing for Primality
-;  skipping as exercises practice number theory, not functional programming..
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+  ((divides? test-divisor n) test-divisor)
+  (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+
+
+
+;
